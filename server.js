1211551fd27b79f8537634603faa824be5d452f5
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2025-07-09 11:58:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dJSON = require("dirty-json");
 const he = require('he');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
